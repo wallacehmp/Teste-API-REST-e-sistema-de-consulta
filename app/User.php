d77c2225c171f5d6ef61123f0b3e9619f1d497bb
wallace346 <wallace346@hotmail.com>
2016-04-07 12:53:53 -0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -7,14 +7,17 @@ class User extends Model
     protected $hidden = ['password'];
     protected $fillable = ['id','usuario','senha'];
 
-    public function getUser($name){
+    public function getUser($name)
+    {
         $user = new User();
         $userTeste = self::all();
+        
         foreach ($userTeste as $key => $value) {
             if($value->email == $name){
                 return $value;
             }
         }
+        
         return false;       
     }    
 }
